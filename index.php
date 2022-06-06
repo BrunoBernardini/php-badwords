3204a2a9d6d4f96d0f7a7afa05aa42135341c529
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-06-06 16:17:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous'/>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Morbius review (censor bad words)</title>
+</head>
+<body class="d-flex flex-column justify-content-center align-items-center my-5">
+  <h1 class="mb-5">Morbius Review</h1>
+  <div class="content w-75 d-flex">
+    <img src="https://appalcinema.it/wp-content/uploads/2022/03/morbius.jpg" alt="MorbiusSweep">
+    <div class="text ms-5 flex-wrap">
+      <div class="review-title d-flex align-items-center mb-5">
+        <h3 class="d-inline mb-0 me-3">The Movie Ever</h3>
+        <span class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </span>
+      </div>
+      <p class="mb-5">
+      Wow. Heartfelt, funny, beautiful, sad, Morbius has it all. Ever since I was a young child I dreamed of the Marvel Legend, Michael Morbius, to make his debut on the big screen. I’d run into the kitchen screaming “father! Father! Have they announced a film about Doctor Micheal Morbius yet?” My father would chuckle, put down his newspaper and say “no son. Not yet.” As my life went on I became afraid I would not live to see the day of Doctor Micheal Morbius’ theatrical debut. But then the fateful day Venom (2018) released, I knew it was possible. 4 years later I run into the kitchen “Father! Father! I am about to see Doctor Micheal Morbius’ big screen debut!” But there was no chuckle. Not rustling of his newspaper. My father had died right before I entered. I had an extra ticket for him. I went to the movie anyways and I loved it! But I stay for the big post credit scene; as any Morbius fan would. And there it was. My own father, who had died and put his soul into the projector, as the vulture. ‘What’s up doc!’ He said. My own father died so he could help make Morbius even better. God love him. God love Morbius.
+      </p>
+      <span><i class="fa-solid fa-heart me-2"></i>8.641 likes</span>
+    </div>
+  </div>
+  <script src="./assets/JS/script.js"></script>
+</body>
+</html>
